eedcabffef4255a9ed59d3ac70c769319b86c201
finesse52 <144905450+finesse52@users.noreply.github.com>
2023-11-08 13:41:18 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -4,9 +4,14 @@ import unittest
 class TestCircle(unittest.TestCase):
     def testPer(self):
         self.assertEqual(perimeter(1/(2*math.pi)), 1)
+        class TestCircle(unittest.TestCase):
+    def testPer(self):
+        self.assertEqual(perimeter(2/(2*math.pi)), 2)
 
     def testArea(self):
         self.assertEqual(area(2 / (math.pi ** 0.5)), float(4))
+            def testArea(self):
+        self.assertEqual(area(4 / (math.pi ** 0.5)), float(2))
 def perimeter(r):
     return 2*math.pi * r
 def area(r):
