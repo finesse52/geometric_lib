e76b87cb4bf382342daacfaddf9e43cd27eee24c
finesse52 <144905450+finesse52@users.noreply.github.com>
2023-12-04 22:29:18 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,17 +1,4 @@
-import unittest
-
-class Testcircle(unittest.TestCase):
-    def testPerimetr(self):
-        self.assertEqual(perimeter(12),48)
-        self.assertEqual(perimeter(100000), 10000*4)
-    def testPerimetr_1(self):
-        self.assertEqual(perimeter(12),48)
-    def testArea(self):
-        self.assertEqual(area(2**0.5), 2)
-    def testArea(self):
-        self.assertEqual(area(100000), 10000000 ** 2)
-
-
+import math
 def area(a):
     return a * a
 def perimeter(a):   
