654accb87cbcb3eb8a1c67d8469e737e7829d6ba
finesse52 <144905450+finesse52@users.noreply.github.com>
2023-12-04 22:33:24 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,6 +1,20 @@
-import math
-def perimeter(r):
-    return 2*math.pi * r
-def area(r):
-    return math.pi * r * r
+def area(a):
+    '''
+    Возвращает площадь квадрата (a, a) в виде числа:
+        Параметры:
+                a (int) : Сторона квадрата
+        Возвращаемое значение:
+                a * a
+    '''
+    return a * a
 
+
+def perimeter(a):
+    '''
+    Возвращает периметр квадрата (a, a) в виде числа:
+        Параметры:
+                a (int) : Сторона квадрата
+        Возвращаемое значение:
+                4 * a
+    '''
+    return 4 * a
